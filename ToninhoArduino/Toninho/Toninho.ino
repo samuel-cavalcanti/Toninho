0bd5c56fd791b4b4b1f17e26fa807006462e794f
samuel-cavalcanti <scavalcanti111@gmail.com>
2017-11-06 22:17:16 -0300

    add defines

--- ToninhoArduino/Toninho/Toninho.ino
+++ ToninhoArduino/Toninho/Toninho.ino
@@ -2,7 +2,27 @@
 #include "ToninhoArduino.h"
 
 
-Toninho* terceiro = new ToninhoArduino(6,5,10,9,0,7,12,13);
+/*
+roteador,
+bateria,
+fone, 
+celular,
+
+*/
+#define trigger 12
+#define echo 13
+#define lineSensorLeft 0
+#define lineSensorRight 7
+#define dcMotorLeftForward 6
+#define dcMotorLeftBackward 5
+#define dcMotorRightForward 10
+#define dcMotorRightBackward 9
+
+
+Toninho* terceiro = new ToninhoArduino(dcMotorLeftForward, dcMotorLeftBackward,
+                                       dcMotorRightForward, dcMotorRightBackward,
+                                       lineSensorLeft,      lineSensorRight,
+                                       trigger,               echo);
 
 void setup() {
   Serial.begin(9600);
@@ -12,10 +32,10 @@ void setup() {
 }
 
 void loop() {
-  //float distance;
-  //terceiro->getDistanceOfProximitySensor(distance);
-  //Serial.print(distance);
-  //Serial.println(" cm");
+  float distance;
+  terceiro->getDistanceOfProximitySensor(distance);
+  Serial.print(distance);
+  Serial.println(" cm");
 
   terceiro->seguirLinha();
   //terceiro->setVelocityInToninho(100,100);
