143cdb84c8ac43d497e2e9c2570eba6641e7950a
samuel-cavalcanti <scavalcanti111@gmail.com>
2017-11-10 16:37:48 -0300

    atualizado Vdc

--- CPPremoteAPI/Vdc/Vdc.cpp
+++ CPPremoteAPI/Vdc/Vdc.cpp
@@ -24,15 +24,18 @@ Vdc::Vdc(std::string serverIP, int serverPort) {
 Vdc::~Vdc() {
 }
 
-void Vdc::conectpiece(std::string nameInVrep, int& nameInRemoteAPI) {
+bool Vdc::conectpiece(std::string nameInVrep, int& nameInRemoteAPI) {
 
     if (simxGetObjectHandle(clientID, (const simxChar*) nameInVrep.c_str(), (simxInt *) & nameInRemoteAPI, (simxInt) simx_opmode_oneshot_wait) != simx_return_ok) {
         if (debug)
             std::cout << nameInVrep << " nao encontrado!" << std::endl;
-
+        nameInRemoteAPI = -1;
+        return false;
     } else {
         if (debug)
             std::cout << "Conectado ao " << nameInVrep << std::endl;
+
+        return true;
     }
 
 }
@@ -46,10 +49,11 @@ T Vdc::degree(T Degree) {
     return (Degree * 3.14159265359) / 180;
 }
 
-template <class T>
-void Vdc::setJointPosition(int joint, T angle) {
-    angle = Vdc::degree(angle);
+void Vdc::setJointPosition(int joint, double angle) {
+    //    angle = Vdc::degree(angle);  caso queria setar em graus ao invez de radianos descomente
     simxSetJointPosition(clientID, joint, angle, simx_opmode_oneshot);
+
+
 }
 
 void Vdc::finish() {
@@ -96,7 +100,7 @@ bool Vdc::readVisionSensor(int sensor, std::vector<float>& VectorAuxValues, int 
     if (simxReadVisionSensor(clientID, sensor, &detectionState, &auxValues, &auxValuesCount, simx_opmode_buffer) == simx_return_ok) {
         for (int i = 0; i < sizeOfauxValues; i++) {
             aux = (float) auxValues[i];
-            
+
             VectorAuxValues.push_back(aux);
         }
         return true;
@@ -105,20 +109,113 @@ bool Vdc::readVisionSensor(int sensor, std::vector<float>& VectorAuxValues, int 
 
 }
 
-bool Vdc::readProximitySensor(int sensor, float &distance){
-     simxFloat coord[3];
-     simxUChar state;
-     //std::cout << "oi " << std::endl;
-      simxReadProximitySensor(clientID, sensor, NULL, NULL, NULL, NULL, simx_opmode_streaming);
-       
+bool Vdc::readProximitySensor(int sensor, float &distance) {
+    simxFloat coord[3];
+    simxUChar state;
+    //std::cout << "oi " << std::endl;
+    simxReadProximitySensor(clientID, sensor, NULL, NULL, NULL, NULL, simx_opmode_streaming);
+
 
     if (simxReadProximitySensor(clientID, sensor, &state, coord, NULL, NULL, simx_opmode_buffer) == simx_return_ok) {
         if (state > 0) {
-            distance = (float)coord[2]; // coord[2]  == z
+            distance = (float) coord[2]; // coord[2]  == z
             return true;
-        } 
+        }
 
     }
-    return  false;
+    return false;
 
+}
+
+int Vdc::getClientID() {
+    return clientID;
+}
+
+bool Vdc::getObjectPosition(int object, float& x, float& y, float& z) {
+
+    simxFloat position[3];
+    
+    int ok = simxGetObjectPosition(clientID, object, -1, position, simx_opmode_streaming);
+
+    while (ok == simx_return_novalue_flag) {
+
+        ok = simxGetObjectPosition(clientID, object, -1, position, simx_opmode_buffer);
+    }
+    
+    if ( ok !=simx_return_ok )
+        return false;
+    
+    x = (float) position[0];
+    y = (float) position[1];
+    z = (float) position[2];
+    
+    return true;
+
+    //std::cout << "x: " << x << " y: " << y << " z: " << z << std::endl;
+
+}
+
+void Vdc::debugerOfReturns(int simxreturn) {
+
+    switch (simxreturn) {
+        case simx_return_novalue_flag:
+            std::cout << "erro 1" << std::endl;
+            break;
+
+        case simx_return_timeout_flag:
+            std::cout << "erro 2" << std::endl;
+            break;
+
+        case simx_return_illegal_opmode_flag:
+            std::cout << "erro 3" << std::endl;
+            break;
+
+        case simx_return_remote_error_flag:
+            std::cout << "erro 4" << std::endl;
+            break;
+
+        case simx_return_split_progress_flag:
+            std::cout << "erro 5" << std::endl;
+            break;
+
+        case simx_return_local_error_flag:
+            std::cout << "erro 6" << std::endl;
+            break;
+
+        case simx_return_initialize_error_flag:
+            std::cout << "erro 7 " << std::endl;
+            break;
+    }
+
+    //          simx_return_ok				=0x000000,
+    //		simx_return_novalue_flag		=0x000001,		/* input buffer doesn't contain the specified command. Maybe you forgot to enable data streaming, or streaming hasn't started yet */
+    //		simx_return_timeout_flag		=0x000002,		/* command reply not received in time for simx_opmode_oneshot_wait operation mode */
+    //		simx_return_illegal_opmode_flag		=0x000004,		/* command doesn't support the specified operation mode */
+    //		simx_return_remote_error_flag		=0x000008,		/* command caused an error on the server side */
+    //		simx_return_split_progress_flag		=0x000010,		/* previous similar command not yet fully processed (applies to simx_opmode_oneshot_split operation modes) */
+    //		simx_return_local_error_flag		=0x000020,		/* command caused an error on the client side */
+    //		simx_return_initialize_error_flag	=0x000040	
+
+
+}
+
+void Vdc::setObjectPosition(int object, float x, float y, float z){
+    const simxFloat position[3] = {x,y,z};
+    simxSetObjectPosition(clientID,object ,-1,position,simx_opmode_oneshot);    
+}
+
+bool Vdc::getJointPosition(int joint, float& position){
+    
+    int ok =  simxGetJointPosition(clientID, joint, &position, simx_opmode_streaming);
+    
+    while (ok == simx_return_novalue_flag) {
+
+        ok =  simxGetJointPosition(clientID, joint, &position, simx_opmode_streaming);
+    }
+    if ( ok != simx_return_ok )
+        return false;
+    
+    return true;
+    
+    
 }
--- CPPremoteAPI/Vdc/Vdc.h
+++ CPPremoteAPI/Vdc/Vdc.h
@@ -14,43 +14,43 @@
 #ifndef VDC_H
 #define VDC_H
 extern "C" {
-  #include <extApi.h>
+#include <extApi.h>
 }
 #include <string>
 #include <iostream>
 #include <vector>
 
-
 class Vdc {
 public:
-    Vdc(std::string serverIP,int serverPort);
-    void conectpiece(std::string nameInVrep, int &nameInRemoteAPI);
+    Vdc(std::string serverIP, int serverPort);
+    bool conectpiece(std::string nameInVrep, int &nameInRemoteAPI);
     void finish();
-    bool connection_is_OK ();
-    bool simulationIsActive ();
+    bool connection_is_OK();
+    bool simulationIsActive();
     void delay(int time);
-    void setclientID(std::string serverIP,int serverPort);
-    void setDebug(bool _debug);
+    void debugerOfReturns (int simxreturn);
     
-    void setJointVelocity(int joint,float velocity);
-    template <class T>
-    void setJointPosition(int joint, T angle);
-    bool readVisionSensor(int sensor , std::vector <float> &auxValues, int sizeOfauxValues);
+    
+    void setclientID(std::string serverIP, int serverPort);
+    void setDebug(bool _debug);
+    void setObjectPosition(int object, float x,float y, float z);
+    void setJointVelocity(int joint, float velocity);
+    void setJointPosition(int joint, double angle);
+    bool getJointPosition(int joint, float& position);
+    bool getObjectPosition(int object, float &x, float &y, float &z);
+    int getClientID();
+    bool readVisionSensor(int sensor, std::vector <float> &auxValues, int sizeOfauxValues);
     bool readProximitySensor(int sensor, float &distance);
    
-    
+ 
     
     Vdc(const Vdc& orig);
     virtual ~Vdc();
 private:
     template <class T>
-     T degree(T Degree);
-    
+    T degree(T Degree);
     int clientID;
     bool debug = false; //deputar tudo !!!
-    
-    
-    
 };
 
 #endif /* VDC_H */
